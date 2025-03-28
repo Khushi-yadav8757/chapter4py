eb1259e92b835cf3a73db48f5fcefe154de468bc
Khushi Yadav <kky957242@gmail.com>
2025-03-29 00:48:34 +0530

    Create tup.py

---
+++ tup.py
@@ -0,0 +1,11 @@
+tup1 = (1,2,3)
+tup2 = (4,5,6)
+result= tup1+tup2               #concatenation
+print(result) 
+
+my_tup = (5,2,3)
+print(my_tup*2)                 #repetition(*)
+
+tup=1,2,3
+a,b,c=tup
+print(a,b,c)
