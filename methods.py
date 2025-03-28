257a9f0daf0ce80f9e173715383508f50cd2731a
Khushi Yadav <kky957242@gmail.com>
2025-03-28 23:53:33 +0530

    Create methods.py

---
+++ methods.py
@@ -0,0 +1,8 @@
+l1 =[34,56,76,22,35,90]
+
+#l1.sort()                            # sort in assending order
+#l1.reverse()                         #it reverse the list
+#l1.insert(2,43)                      #insert a new thing in list
+#l1.pop(2)                            #delete from index
+l1.remove(22)                         #remove from the list 
+print(l1)
