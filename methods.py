1cb8c4fa7a8e9a13253cf6eae73f3a98a8f1f95e
Khushi Yadav <kky957242@gmail.com>
2025-07-26 23:54:40 +0530

    Update methods.py

--- methods.py
+++ methods.py
@@ -5,4 +5,4 @@ l1 =[34,56,76,22,35,90]
 #l1.insert(2,43)                      #insert a new thing in list
 #l1.pop(2)                            #delete from index
 l1.remove(22)                         #remove from the list 
-print(l1)
+print(l1)  
